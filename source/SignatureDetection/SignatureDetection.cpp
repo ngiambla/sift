221ac5555a6302784ed8b9a882204093b6a40b07
Nicholas Giamblanco <gdev.engineering@gmail.com>
2018-02-27 16:16:44 -0500

    ~ added instruction couting.

--- source/SignatureDetection/SignatureDetection.cpp
+++ source/SignatureDetection/SignatureDetection.cpp
@@ -1,18 +1,32 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+#include <map>
 
 using namespace llvm;
 namespace {
 	struct SignatureDetection : public FunctionPass {
+		std::map<std::string, int> opCounter;
 		static char ID;
 		SignatureDetection() : FunctionPass(ID) {}
 
 		bool runOnFunction(Function &F) override {
-			errs() << "#############################\n";
-			errs() << "~ Identifying Signatures\n";
+			errs() << "~ Identifying Signature for: ";
 			errs().write_escaped(F.getName()) << '\n';
-			errs() << "#############################\n";
+			for(Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
+				BasicBlock &b = *bb;
+				for(BasicBlock::iterator i = b.begin(), e2 = b.end(); i != e2; ++i) {
+					if ( opCounter.find(i->getOpcodeName()) == opCounter.end() ) {
+						opCounter[i->getOpcodeName()] = 1; //New OpCode in the list
+					} else {
+						opCounter[i->getOpcodeName()] += 1; //Incrementing the old one
+					}
+				}
+			}
+			errs() << "~ Checking OP usages...";
+			for(auto const& key : opCounter) {
+				errs() << key.first << " : " << key.second << "\n";
+			}
 			return false;
 		}
 	};
